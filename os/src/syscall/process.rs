4283d36d75ff35bd81a1560d7bda863f07e3f0be
Dynamic_Pigeon <3040313336@qq.com>
2024-10-12 20:17:24 +0800

    update

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,9 +1,7 @@
 //! Process management syscalls
 use crate::{
-    config::MAX_SYSCALL_NUM, syscall::add_syscall_time, task::{exit_current_and_run_next, get_running_task_info, get_running_task_start_time, suspend_current_and_run_next, TaskStatus}, timer::{get_time_ms, get_time_us}
+    config::MAX_SYSCALL_NUM, task::{exit_current_and_run_next, get_running_task_info, get_running_task_start_time, suspend_current_and_run_next, TaskStatus}, timer::{get_time_ms, get_time_us}
 };
-
-use super::{SYSCALL_EXIT, SYSCALL_TASK_INFO, SYSCALL_YIELD, SYSCALL_GET_TIME};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -37,9 +35,6 @@ impl TaskInfo {
 /// task exits and submit an exit code
 pub fn sys_exit(exit_code: i32) -> ! {
     trace!("[kernel] Application exited with code {}", exit_code);
-
-    add_syscall_time(SYSCALL_EXIT);
-
     exit_current_and_run_next();
     panic!("Unreachable in sys_exit!");
 }
@@ -47,9 +42,6 @@ pub fn sys_exit(exit_code: i32) -> ! {
 /// current task gives up resources for other tasks
 pub fn sys_yield() -> isize {
     trace!("kernel: sys_yield");
-
-    add_syscall_time(SYSCALL_YIELD);
-
     suspend_current_and_run_next();
     0
 }
@@ -57,8 +49,6 @@ pub fn sys_yield() -> isize {
 /// get time with second and microsecond
 pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     trace!("kernel: sys_get_time");
-
-    add_syscall_time(SYSCALL_GET_TIME);
 
     let us = get_time_us();
     unsafe {
@@ -75,11 +65,13 @@ pub fn sys_task_info(ti: *mut TaskInfo) -> isize {
     trace!("kernel: sys_task_info");
     // 取得当前任务的开始时间
     let start_time = get_running_task_start_time();
-
-    add_syscall_time(SYSCALL_TASK_INFO);
-
     let task_info = get_running_task_info();
     let mut task_info = task_info.exclusive_access();
+
+    if task_info.status != TaskStatus::Running {
+        return -1;
+    }
+
     let cur_time = get_time_ms();
     task_info.time = cur_time - start_time;
     unsafe {
