89dda682604a69e97bc207887eaa6b1e776422d9
Dynamic_Pigeon <3040313336@qq.com>
2024-10-28 19:58:59 +0800

    update

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -174,63 +174,11 @@ impl TaskControlBlock {
 
     /// Spawn a new process
     pub fn spawn(self: &Arc<TaskControlBlock>, elf_data: &[u8]) -> Arc<TaskControlBlock> {
-        // ---- hold parent PCB lock
         let mut parent_inner = self.inner_exclusive_access();
-        // memory_set with elf program headers/trampoline/trap context/user stack
-        let (memory_set, user_sp, entry_point) = MemorySet::from_elf(elf_data);
-        let trap_cx_ppn = memory_set
-            .translate(VirtAddr::from(TRAP_CONTEXT_BASE).into())
-            .unwrap()
-            .ppn();
-        // alloc a pid and a kernel stack in kernel space
-        let pid_handle = pid_alloc();
-        let kernel_stack = kstack_alloc();
-        let kernel_stack_top = kernel_stack.get_top();
-        let task_control_block = Arc::new(TaskControlBlock {
-            pid: pid_handle,
-            kernel_stack,
-            inner: unsafe {
-                UPSafeCell::new(TaskControlBlockInner {
-                    trap_cx_ppn,
-                    base_size: user_sp,
-                    task_cx: TaskContext::goto_trap_return(kernel_stack_top),
-                    task_status: TaskStatus::Ready,
-                    memory_set,
-                    parent: Some(Arc::downgrade(self)),
-                    children: Vec::new(),
-                    exit_code: 0,
-                    fd_table: vec![
-                        // 0 -> stdin
-                        Some(Arc::new(Stdin)),
-                        // 1 -> stdout
-                        Some(Arc::new(Stdout)),
-                        // 2 -> stderr
-                        Some(Arc::new(Stdout)),
-                    ],
-                    heap_bottom: user_sp,
-                    program_brk: user_sp,
-                    task_info: TaskInfo::new(),
-                    start_time: 0,
-                    priority: 16,
-                    stride: 0,
-                    fd_name_table: BTreeMap::new(),
-                })
-            },
-        });
-        // add child
-        parent_inner.children.push(task_control_block.clone());
-        // initialize trap_cx
-        let trap_cx = task_control_block.inner_exclusive_access().get_trap_cx();
-        *trap_cx = TrapContext::app_init_context(
-            entry_point,
-            user_sp,
-            KERNEL_SPACE.exclusive_access().token(),
-            kernel_stack_top,
-            trap_handler as usize,
-        );
-        // return
-        task_control_block
-        // ---- release parent PCB
+        let child = Arc::new(TaskControlBlock::new(elf_data));
+        child.inner_exclusive_access().parent = Some(Arc::downgrade(self));
+        parent_inner.children.push(child.clone());
+        child
     }
 
     /// Load a new elf to replace the original application address space and start execution
